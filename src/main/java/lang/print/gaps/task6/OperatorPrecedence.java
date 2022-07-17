910ffb849717cd84b5300142302bb3dc8658fe63
MuhammadjonR <100756194+MuhammadjonR@users.noreply.github.com>
2022-07-17 21:18:00 +0500

    Update OperatorPrecedence.java

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -1,18 +1,18 @@
 package lang.print.gaps.task6;
 
-public class PrePostIncrement {
+public class OperatorPrecedence {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        int c = 12;
-        int d = 15;
+        int first = 1;
+        int second = 2;
+        int eight = (3 + first) * second;
+        System.out.println(eight);//eight is expected to be 8
+        boolean firstBool = false;
+        boolean secondBool = false;
+        boolean thirdBool = 1 == 1;
+        boolean fourthBool = 4 <= 101;
+        int varSix = 17;
 
-        // use post-increment operator in the mathematical expression  
-        int x =   a++   + b +   ++c   + d;
-        System.out.println("Expected x is 40, x = " + x);
-        // print the updated value of a, b, c, and d  
-        System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
-
+        System.out.println(firstBool && (secondBool || thirdBool) && fourthBool || (varSix = 5) < 0);
+        System.out.println(varSix);
     }
 }
-
